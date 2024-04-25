016b023beb8153dae9f55c3150a04ec847b0e36c
JithuPrabhakar <join.jitz@gmail.com>
2024-04-25 11:32:11 +0530

    completed department

---
+++ client/src/screens/Department/MsgAdmin.jsx
@@ -0,0 +1,171 @@
+import { useTheme, Box, Grid, TextField, Button } from '@mui/material'
+import { useDispatch } from 'react-redux'
+
+const MsgAdmin = () => {
+  const { palette } = useTheme()
+  const dispatch = useDispatch()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    const message = {
+      item: data.get('item'),
+      type: data.get('type'),
+      message: data.get('message'),
+      estimate: data.get('estimate'),
+    }
+    console.log(message)
+
+    event.currentTarget.reset()
+
+    // Assuming `register` and `setCredentials` are defined elsewhere
+    // const res = await register(message).unwrap()
+    // dispatch(setCredentials({ ...res }))
+  }
+
+  return (
+    <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            InputProps={{ sx: { color: 'white' } }}
+            InputLabelProps={{
+              sx: {
+                '&.Mui-focused': {
+                  color: palette.primary.light,
+                },
+                '&.MuiInputLabel-root': {
+                  color: palette.grey[300],
+                },
+              },
+            }}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '&:hover fieldset': {
+                  borderColor: palette.primary.light,
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: palette.primary.light,
+                },
+              },
+            }}
+            autoComplete='item'
+            name='item'
+            required
+            fullWidth
+            id='item'
+            label='Item'
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            InputProps={{ sx: { color: 'white' } }}
+            InputLabelProps={{
+              sx: {
+                '&.Mui-focused': {
+                  color: palette.primary.light,
+                },
+                '&.MuiInputLabel-root': {
+                  color: palette.grey[300],
+                },
+              },
+            }}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '&:hover fieldset': {
+                  borderColor: palette.primary.light,
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: palette.primary.light,
+                },
+              },
+            }}
+            required
+            fullWidth
+            id='type'
+            label='Complaint/New'
+            name='type'
+            autoComplete='off'
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            InputProps={{ sx: { color: 'white' } }}
+            InputLabelProps={{
+              sx: {
+                '&.Mui-focused': {
+                  color: palette.primary.light,
+                },
+                '&.MuiInputLabel-root': {
+                  color: palette.grey[300],
+                },
+              },
+            }}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '&:hover fieldset': {
+                  borderColor: palette.primary.light,
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: palette.primary.light,
+                },
+              },
+            }}
+            required
+            fullWidth
+            id='message'
+            label='Message'
+            name='message'
+            autoComplete='off'
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            InputProps={{ sx: { color: 'white' } }}
+            InputLabelProps={{
+              sx: {
+                '&.Mui-focused': {
+                  color: palette.primary.light,
+                },
+                '&.MuiInputLabel-root': {
+                  color: palette.grey[300],
+                },
+              },
+            }}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                '&:hover fieldset': {
+                  borderColor: palette.primary.light,
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: palette.primary.light,
+                },
+              },
+            }}
+            required
+            fullWidth
+            id='estimate'
+            label='Estimate Amount'
+            name='estimate'
+            autoComplete='off'
+          />
+        </Grid>
+      </Grid>
+      <Button
+        type='submit'
+        fullWidth
+        variant='contained'
+        sx={{
+          background: palette.primary.light,
+          '&:hover': { background: palette.primary[300] },
+          mt: 3,
+        }}
+      >
+        Submit
+      </Button>
+    </Box>
+  )
+}
+
+export default MsgAdmin
--- client/src/screens/DeptHeadPage.jsx
+++ client/src/screens/DeptHeadPage.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
-import { useTheme, Typography, Box } from '@mui/material'
+import { useTheme, Typography, Box, Button } from '@mui/material'
 import StudentForm from './Department/StudentForm'
+import MsgAdmin from './Department/MsgAdmin'
 
 const DeptHeadPage = () => {
   const [departmentName, setDepartmentName] = useState('')
+  const [showStudentForm, setShowStudentForm] = useState(false)
+  const [showMsgAdminForm, setShowMsgAdminForm] = useState(false)
   const { palette } = useTheme()
+
+  const handleShowStudentForm = () => {
+    setShowStudentForm(true)
+    setShowMsgAdminForm(false)
+  }
+
+  const handleShowMsgAdminForm = () => {
+    setShowStudentForm(false)
+    setShowMsgAdminForm(true)
+  }
 
   return (
     <Box sx={{ mt: 4, textAlign: 'center' }}>
@@ -14,7 +27,32 @@ const DeptHeadPage = () => {
       <Typography variant='h3' sx={{ color: palette.primary.light, mt: 2 }}>
         Student Fees Details
       </Typography>
-      <StudentForm />
+      <Button
+        onClick={handleShowStudentForm}
+        size='large'
+        variant='contained'
+        sx={{
+          m: 2,
+          backgroundColor: palette.primary.main,
+        }}
+        disabled={showStudentForm}
+      >
+        Add student fee
+      </Button>
+      <Button
+        onClick={handleShowMsgAdminForm}
+        size='large'
+        variant='contained'
+        sx={{
+          m: 2,
+          backgroundColor: palette.primary.main,
+        }}
+        disabled={showMsgAdminForm}
+      >
+        Ask for repairs to admin
+      </Button>
+      {showStudentForm && <StudentForm />}
+      {showMsgAdminForm && <MsgAdmin />}
     </Box>
   )
 }
